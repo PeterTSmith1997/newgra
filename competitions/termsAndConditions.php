fcb2e18933bd23fbdcb43549c1be9dce2f1510a6
Petersweb <peter.t.smith@northumbria.ac.uk>
2018-08-24 16:28:34 +0000

    T&C'S update

--- competitions/termsAndConditions.php
+++ competitions/termsAndConditions.php
@@ -57,64 +57,22 @@ include '../includes/navBar.php'
     <p>Please read the competition rules carefully.  If you enter the our competition we will assume that you have done so and that you agree to them.</p>
 
     <ol class="comps">
-
-        <li>The competition is open to everyone who has liked our page,
-
-            www.facebook.com/GenealogyResearchAssistant/ or are in our group
-
-            www.facebook.com/groups/15597179680523/ with the exception of Genealogy
-
-            Research Assistance Admins, their families, close friends or anyone associated with petersweb.</li>
-
-        <li> No purchase is necessary.</li>
-
+        <li>The competition is open to everyone who has liked our page, www.facebook.com/GenealogyResearchAssistant/ or are in our group
+            www.facebook.com/groups/15597179680523/ with the exception of Genealogy Research Assistance Admins, their families, close friends
+            or anyone associated with petersweb.</li>
+        <li>No purchase is necessary.</li>
         <li>Only one entry per person</li>
-
-        <li>To enter the competition you need to answer all 3 questions correctly.  One
-
-            question will be posted daily starting 30 March 2018 and concluding on 2 April 2018.
-
-            Details of where to send your answers will be posted on the final day.</li>
-
-        <li>The closing date for all entries is 23.59 9 April 2018</li>
-
-        <li> If you win the competition you will be notified by email within 14 working days after
-
-            the close of the competition. If you do not respond within 48 hours of being
-
-            notified, your prize will be forfeited.  Genealogy Research Assistance reserve the
-
-            right to choose another winner.  The judges decision is final and no further
-
-            correspondence will be entered into.</li>
-
-        <li>  The winners names will be posted on our facebook page once all notifications have
-
-            been verified.</li>
-
+        <li>To enter the competition you need to answer all 3 questions correctly. One question will be posted daily starting 25 August 2018 and concluding           on 27 August 2018. Details of where to send your answers will be posted on the final day.</li>
+        <li>The closing date for all entries is 23.59 1 September 2018.</li>
+        <li>If you win the competition you will be notified by email within 14 working days after the close of the competition. If you do not respond within 48 hours of being notified, your prize will be forfeited. Genealogy Research Assistance reserve the right to choose another winner. The judges decision is final and no further correspondence will be entered into.</li>
+        <li>The winners names will be posted on our facebook page once all notifications have been verified and on our website.</li>
         <li>The prize is not transferable to another person.</li>
-
-        <li>No part of the prize is exchangeable for cash or any other prize</li>
-
-        <li>If an advertised prize is not available we reserve the right to offer another prize of
-
-            the equal or greater value.</li>
-
-        <li> Incorrectly completed entry forms will be disqualified.</li>
-
-        <li> At the close of the competition all personal details of the entrants will be deleted
-
-            and not passed to a third party with the exception of the winners of prizes one to
-
-            four whose details will be passed to the relevant sites for activation.
-
-        </li>
-
+        <li>No part of the prize is exchangeable for cash or any other prize.</li>
+        <li>If an advertised prize is not available we reserve the right to offer another prize of the equal or greater value.</li>
+        <li>Incorrectly completed entry forms will be disqualified.</li>
+        <li>We will collect your email address and name in order to contact you if you win the competition.  The details of the winner will be passed on to FindmyPast so that you prize can be activated.  At the close of the competition all details will be deleted from our system.</li>
         <li>Genealogy Research Assistance reserve the right to amend the rules at any time.</li>
-
-        <li> Genealogy Research Assistance will endeavour to send all prizes within one
-
-            month of the closure of the competition but cannot guarantee delivery dates.</li>
+        <li>Genealogy Research Assistance will endeavour to send all prizes within one month of the closure of the competition but cannot guarantee delivery dates.</li>
 
     </ol>
 
